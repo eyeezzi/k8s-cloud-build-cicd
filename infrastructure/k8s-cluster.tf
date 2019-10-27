61cea199deb747cc6fa53b073f7a23bc4c58b7be
Uzziah Eyee <uzziah.eyee@shopify.com>
2019-10-27 16:22:02 -0700

    Created 2 separate clusters for staging and production.

--- infrastructure/k8s-cluster.tf
+++ infrastructure/k8s-cluster.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_kubernetes_cluster" "hive-staging" {
-  name    = "hive"
+  name    = "hive-staging"
   region  = "nyc1"
   version = "1.15.5-do.0"
 
@@ -73,21 +73,6 @@ resource "digitalocean_record" "staging-polite-opsolute-com" {
 //   region     = "${digitalocean_kubernetes_cluster.hive.region}"
 // }
 
-# @inconvenience: the local gcloud has to be authenticated with the project.
-
-# Save the kubeconfig file to access the cluster.
-# Encrypt it with the Google KMS key
-resource "local_file" "kubeconfig" {
-  sensitive_content = digitalocean_kubernetes_cluster.hive.kube_config.0.raw_config
-  filename          = "../secrets/kubeconfig.enc"
-
-  provisioner "local-exec" {
-    command = "echo $KUBECONFIG | gcloud kms encrypt --project=k8s-cicd-251209 --plaintext-file=- --ciphertext-file=- --location=global --keyring=${google_kms_key_ring.polite-staging.name} --key=${google_kms_crypto_key.polite-staging-default.name} | base64 > ${self.filename}"
-    environment = {
-      KUBECONFIG = "${self.sensitive_content}"
-    }
-  }
-}
 
 # Allow the DigitalOcean cluster to pull images from the Google Container Registry.
 # A GCP Project has 1 GCR Registry per region, which is backed by a GCS Bucket.
@@ -110,68 +95,19 @@ resource "google_service_account_key" "digital-ocean-cluster" {
   service_account_id = google_service_account.digital-ocean-cluster.name
 }
 
-# Create 2 Virtual Clusters to represent the 2 deployment environments: Staging and Production
-// resource "kubernetes_namespace" "polite" {
-//   metadata {
-//     name = "staging"
-//   }
-// }
-
-// resource "kubernetes_namespace" "production" {
-//   metadata {
-//     name = "production"
-//   }
-// }
-
-/*----------------------
-# TODO (IMPORTANT): Automate the creation of GCR ImagePullSecret.
-# For some reason, I cannot create the GCR ImagePullSecret using TF resources.
-# So I have created it manually following the instructions here: 
-#   - http://docs.heptio.com/content/private-registries/pr-gcr.html
-# ISSUE: https://github.com/terraform-providers/terraform-provider-kubernetes/issues/611
-
-# Create an ImagePullSecret from the GCR service account key.
-# Specify this ImagePullSecret in every PodSpec that pulls images from GCR
-locals {
-  dockerconfigjson = {
-    "https://gcr.io" = {
-      email    = "youremail@example.com" # required but unused
-      username = "_json_key"             # must be _json_key
-      // password = base64decode(google_service_account_key.digital-ocean-cluster.private_key)
-      password = "test"
-    }
-  }
+# Point all subdomains to the traefik loadbalancer IP
+#   staging.app-1.myorg.com
+#   staging.app-n.myorg.com     ----> 5.5.5.5 (traefik service)
+# NOTE: 
+#   DigitalOcean always provisions a new LoadBalancer for a loadbalancer-type K8s service.
+#   This new LB has a new IP, and you cannot preset the IP through the service definition (like in GKE).
+#   So whenever the traefik service LB IP changes, YOU MUST update this record accordingly.
+# ISSUE:
+#   - https://www.digitalocean.com/community/questions/how-to-set-static-ip-for-loadbalancer-in-kubernetes
+#   - https://www.digitalocean.com/community/questions/how-to-reuse-do-loadbalancer-previously-created-through-kubernetes
+resource "digitalocean_record" "wildcard-polite-opsolute-com" {
+  domain = digitalocean_domain.polite-opsolute-com.name
+  type   = "A"
+  name   = "*"
+  value  = "45.55.106.142"
 }
-resource "kubernetes_secret" "gcr-imagepullsecret-staging" {
-  metadata {
-    name      = "gcr-imagepullsecret-staging"
-    namespace = kubernetes_namespace.staging.metadata.0.name
-  }
-  data = {
-    ".dockerconfigjson" = jsonencode(local.dockerconfigjson)
-  }
-  type = "kubernetes.io/dockerconfigjson"
-}
-output gcr-private-key {
-  value = kubernetes_secret.gcr-imagepullsecret-staging.data
-}
-------------------*/
-
-// TODO: GCR imagePullSecret for production env
-
-# Add imagePullSecret to K8s Deployment template file
-// TODO
-
-// # Add the GCR ImagePullSecret to the default ServiceAccount in the Staging Namespace.
-// # DRAWBACK: TF cannot edit the default ServiceAccount in a namespace WITHOUT first importing it.
-// #   This limitation makes manual intervention a neccessity when deploying infrastructure, so ignore this approach.
-// # issue: https://github.com/terraform-providers/terraform-provider-kubernetes/issues/302
-// resource "kubernetes_service_account" "default-serviceaccount-staging" {
-//   metadata {
-//     name = "default"
-//     namespace = kubernetes_namespace.staging.metadata.0.name
-//   }
-//   image_pull_secret {
-//     name = kubernetes_secret.gcr-imagepullsecret-staging.metadata.0.name
-//   }
-// }
