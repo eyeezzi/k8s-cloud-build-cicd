6a0fadaa5f5d4a436482c31bdf92bbcf1d56d640
Uzziah Eyee <uzziah.eyee@shopify.com>
2019-11-02 17:30:47 -0700

    Added digital ocean resources: staging and prod clusters, and DNS records.

---
+++ infrastructure/do-k8s-cluster.tf
@@ -0,0 +1,48 @@
+resource "digitalocean_kubernetes_cluster" "hive-staging" {
+  name    = "hive-staging"
+  region  = "nyc1"
+  version = "1.15.5-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+    tags       = ["bees"]
+  }
+}
+
+resource "digitalocean_kubernetes_cluster" "hive-prod" {
+  name    = "hive-prod"
+  region  = "nyc1"
+  version = "1.15.5-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+    tags       = ["bees"]
+  }
+}
+
+# Point the naked domain to the cluster load balancer. This will be our production endpoint.
+resource "digitalocean_domain" "polite-opsolute-com" {
+  name       = "polite.opsolute.com"
+  ip_address = "157.230.202.16"
+}
+
+resource "digitalocean_record" "staging-polite-opsolute-com" {
+  domain = digitalocean_domain.polite-opsolute-com.name
+  type   = "A"
+  name   = "staging"
+  value  = "134.209.130.67"
+}
+
+# Required for Traefik to use Letsencrypt TLS-ALPN-01 challenge
+resource "digitalocean_record" "polite-opsolute-com-caa" {
+  domain = digitalocean_domain.polite-opsolute-com.name
+  type   = "CAA"
+  name   = "@"
+  value  = "letsencrypt.org."
+  tag    = "issue"
+  flags  = 0
+}
